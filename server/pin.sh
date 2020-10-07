14dcc7b93e4364965ab658433df735b47dee38ef
iwita <atzenetopoulos@gmail.com>
2020-10-07 17:33:02 +0300

    added variable pod name

--- server/pin.sh
+++ server/pin.sh
@@ -3,7 +3,7 @@ POD_NAME=$1
 CORES=$2
 
 #echo $CORES
-CONTAINER_ID=$(docker ps | awk '{printf $1; for (i=3;i<=NF;i++) printf FS$i; print NL}' | grep relaxed | awk '{print $1}')
+CONTAINER_ID=$(docker ps | awk '{printf $1; for (i=3;i<=NF;i++) printf FS$i; print NL}' | grep ${1} | awk '{print $1}')
 
 #echo "Found Container: $CONTAINER_ID"
 docker update --cpuset-cpus ${CORES} ${CONTAINER_ID}
