971de72a4e85d15e326a99f4a11ddeebe527330b
iwita <atzenetopoulos@gmail.com>
2020-10-09 11:25:36 +0300

    changed grep exp in pin.sh

--- server/pin.sh
+++ server/pin.sh
@@ -5,7 +5,7 @@ CORES=$2
 
 #echo "POD name: $POD_NAME"
 #echo "Cores to pin: $CORES"
-CONTAINER_ID=$(docker ps | awk '{printf $1; for (i=3;i<=NF;i++) printf FS$i; print NL}' | grep "${POD_NAME}" | awk '{print $1}')
+CONTAINER_ID=$(docker ps | awk '{printf $1; for (i=3;i<=NF;i++) printf FS$i; print NL}' | grep -Ei "k8s_server.*${POD_NAME}" | awk '{print $1}')
 
 #echo "Found Container: $CONTAINER_ID"
 docker update --cpuset-cpus ${CORES} ${CONTAINER_ID}
